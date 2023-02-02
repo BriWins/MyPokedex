f8305ef508d33c59459fbad13becfef8a62ac9b4
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-01 20:58:10 -0600

    add functions
    added functions to add arrays items and return them. Create IIFE to prevent globalness

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-/* Testing */
-
-const pokemonList = [
-    {name: "Pikachu", height: 1, type: ["static","lighteningrod"]},
-    {name: "Pidgeot", height: 0.5, type: ["tackle","twister"]},
-    {name: "Pichu", height: 0.3, type: ["kindness","charm"]}
-];
-
-for ( let i = 0; i < pokemonList.length; i++ ) {
-  
-    if ( pokemonList[i].height >= 1 ) {
-        document.write("Name: " + pokemonList[i].name  + " " + "Height: " + pokemonList[i].height + "- thats one big ole pokemon! </br>");
-    } else {
-        document.write("Name: " + pokemonList[i].name  + " " + "Height: " + pokemonList[i].height + " </br>");
+/* IIFE to prevent globalness */
+let pokemonRepository = (function () {
+    const pokemonList = [
+        {name: "Pikachu", height: 1, type: ["static","lighteningrod"]},
+        {name: "Pidgeot", height: 0.5, type: ["tackle","twister"]},
+        {name: "Pichu", height: 0.3, type: ["kindness","charm"]}
+    ];
+    
+    /* add() adds a new pokemon item into array */
+    function add(pokemon) {
+        return pokemonList.push(pokemon);
     }
-}
+
+    /* getAll() returns each item in array */
+    function getAll() {
+        return pokemonList;
+    }
+   
+    return {
+        add: add,
+        getAll: getAll,
+    };
+    
+})();
+
+/* forEach() loop returns array items outside of expression */
+pokemonRepository.getAll().forEach(function(pokemon) {
+    if ( pokemon.height >= 1 ) {
+        document.write("</p> Name: " + pokemon.name  + " " + "Height: " + pokemon.height + "- thats one big ole pokemon! </br>");
+    } else {
+        document.write("</p> Name: " + pokemon.name  + " " + "Height: " + pokemon.height + " </br>");
+}});
+

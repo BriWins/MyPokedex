f6f8adf89f38dc1c18aea4d4682424ee0fe1dc38
BriWins <95152254+BriWins@users.noreply.github.com>
2023-02-03 19:16:54 -0600

    Created two functions
    One function displays item details on demand, the second list for the click from user

--- index.js
+++ index.js
@@ -27,20 +27,38 @@ let pokemonRepository = (function () {
         const listItem = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
-        button.classList.add("btn-styling");
+        button.classList.add("btn-display");
         listItem.appendChild(button);
         listDisplay.appendChild(listItem);
+        listenForClick();
+    }
+
+/* showDetails() display specific details after details have been fully loaded */
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+/* listenForClick listening for clicks on button that will display a pokemon item */
+    function listenForClick(pokemon) {
+        let button = document.querySelector("button")
+        button.addEventListener("click", function (event) {
+            let target = event.target;
+            target.classList("btn-display");
+            showDetails(pokemon);
+        })
     }
 
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
+        showDetails: showDetails,
+        listenForClick: listenForClick,
     }; 
 })();
 
 /* forEach() loop returns array items outside of expression by using getAll() */
 pokemonRepository.getAll().forEach(function(pokemon) {
-   pokemonRepository.addListItem(pokemon);  // calls needed function from within expression
+   pokemonRepository.addListItem(pokemon);  // this function  is called from within expression
 });
 
